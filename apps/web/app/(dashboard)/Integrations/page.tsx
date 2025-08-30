f7bac933352445cceeb376ce7b84a1e9f2fdd593
Devendra Rathore <devendrarathore7733.com>
2025-08-30 18:04:23 +0530

    dashboard layout

---
+++ apps/web/app/(dashboard)/Integrations/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <div>
+            Integration
+        </div>
+    );
+};
+
+export default Page;
---
+++ apps/web/app/(dashboard)/plugins/vapi/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+    return ( <p>Vapi Plugins</p>   );
+};
+
+export default Page;
