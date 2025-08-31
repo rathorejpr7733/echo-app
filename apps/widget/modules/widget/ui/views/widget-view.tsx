5579ff2981252be31c238218cab82c134cab37b2
Devendra Rathore <devendrarathore7733.com>
2025-08-31 20:42:39 +0530

    widget screen routing using jotai

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,24 +1,34 @@
 "use client";
 
-// import { WidgetFooter } from "../components/widget-footer";
-// import { WidgetHeader } from "../components/widget-header";
-import { WidgetAuthScreen } from "../screens/widget-auth-screen";
-
+import { useAtomValue } from "jotai";
+import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen";
+import{ screenAtom } from "@/modules/widget/atoms/widget-atoms";
 
 interface Props {
     organizationId: string;
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
+    const screen = useAtomValue(screenAtom);
+
+
+    const screenComponents = {
+      error: <p>TODO:Error</p>,
+      loading: <p>TODO:loading</p>,
+      selection: <p>TODO:selection</p>,
+      voice: <p>TODO:voice</p>,
+      auth: <p>TODO:<WidgetAuthScreen/></p>,
+      inbox: <p>TODO:inbox</p>,
+      chat: <p>TODO:chat</p>,
+      contact: <p>TODO:contact</p>,
+          }
+      
+      
 
     return(
         // TODO: Confirm wheather or not  min-h-screen and min-w-screen is needed here
         <main className=" min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-  
-        <WidgetAuthScreen />
-       
-
-           {/* <WidgetFooter /> */}
+                {screenComponents[screen]}
 
         </main>
     );
