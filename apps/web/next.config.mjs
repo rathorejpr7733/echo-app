f65fad732c33a61aa38e41aed7d340cf000efc47
Devendra Rathore <devendrarathore7733.com>
2025-09-28 15:45:39 +0530

    source page destination updated

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,3 +1,4 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
@@ -6,11 +7,47 @@ const nextConfig = {
     return[
       {
         source: '/',
-        destination: '/dashboard/conversations',
-        permanent:false,
+        destination: '/conversations',
+        permanent: false,
       },
     ]
   }
 };
 
 export default nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   transpilePackages: ["@workspace/ui"],
+//   devIndicators: false,
+//   async redirects() {
+//     return[
+//       {
+//         source: '/',
+//         destination: '/dashboard/conversations',
+//         permanent:false,
+//       },
+//     ]
+//   }
+// };
+
+// export default nextConfig;
