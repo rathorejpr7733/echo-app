8f36262af085e3ff95701c053f17fb342fbb9be7
Devendra Rathore <devendrarathore7733.com>
2025-09-28 14:07:09 +0530

    update customization

--- apps/web/app/(dashboard)/Customization/page.tsx
+++ apps/web/app/(dashboard)/Customization/page.tsx
@@ -1,3 +1,4 @@
+
 import { Protect } from "@clerk/nextjs";
 import { PremiumFeatureOverlay } from "@/modules/billing/ui/components/premium-feature-overlay";
 import { CustomizationView } from "@/modules/customization/ui/views/customization-view";
